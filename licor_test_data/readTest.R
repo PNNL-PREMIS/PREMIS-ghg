d541fd2ef7862b1e2e5c8cd54bcaea4653a9457b
Stephanie Pennington <spenn1@terpmail.umd.edu>
2018-03-08 11:10:01 -0500

    Remove unwanted charcters

--- licor_test_data/readTest.R
+++ licor_test_data/readTest.R
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
-#read test file
-
-print("hello")
-=======
 # Read a test file - parse it for "Label" and "Lin_Flux" lines 
 # Stephanie Pennington March 2018
 
@@ -13,4 +8,4 @@ read_licor_data <- function(filename) {
 
 
 read_licor_data("Test51117.81x")
->>>>>>> 4dee3d353af58e00bb2d5980fddb5a770bb9e6e3
+
