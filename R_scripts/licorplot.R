9b352ba8635e43d489cf6c5528da71b4c563a940
Stephanie Pennington <spenn1@terpmail.umd.edu>
2018-05-01 09:55:39 -0400

    Group data/take daily averages

--- R_scripts/licorplot.R
+++ R_scripts/licorplot.R
@@ -13,8 +13,12 @@ library(readr)
 source("licordat.R")
 licorDat <- read_dir("../licor_data/")
 collarDat <- read_csv("../design/cores_collars.csv")
+plots <- read_csv("../design/plots.csv")
 dat <- left_join(licorDat, collarDat, by = "Collar") %>% 
   rename(Origin_Plot = Plot)
+dat <- left_join(dat, plots, by = c("Origin_Plot" = "Plot")) %>%
+  rename(Origin_Salinity = Salinity, Origin_Elevation = Elevation)
+
 
 # For any transplant core X, we know (in "Core_placement") the hole in which it ended up (or
 # rather, the core number of the hole). We actually need to know the plot. So create a lookup
@@ -22,32 +26,39 @@ dat <- left_join(licorDat, collarDat, by = "Collar") %>%
 lookup_table <- collarDat %>% 
   select(Collar, Destination_Plot = Plot)
 
-#lookup_table <- select(collarDat, Core, Lookup_Plot = Plot)
-
 # ...and then merge back into main data frame. Now "Lookup_Plot" holds the plot info for
 # where each core ENDED UP, not where it STARTED
+
 dat <- left_join(dat, lookup_table, by = c("Core_placement" = "Collar"))
+# remove duplicate variables
+dat$Longitude = NULL
+dat$Latitude = NULL
+dat$Plot_area_m2 = NULL
 
-# Extract salinity and elevation information
-dat$Dest_Salinity <- substr(dat$Destination_Plot, 1, 1)
-dat$Dest_Salinity <- factor(dat$Dest_Salinity, levels = c("H", "M", "L"))
-dat$Dest_Elevation <- substr(dat$Destination_Plot, 3, 3)
-dat$Dest_Elevation <- factor(dat$Dest_Elevation, levels = c("L", "M", "H"))
-
-# Extract salinity and elevation information
-dat$Origin_Salinity <- substr(dat$Origin_Plot, 1, 1)
-dat$Origin_Salinity <- factor(dat$Origin_Salinity, levels = c("H", "M", "L"))
-dat$Origin_Elevation <- substr(dat$Origin_Plot, 3, 3)
-dat$Origin_Elevation <- factor(dat$Origin_Elevation, levels = c("L", "M", "H"))
-
+dat <- left_join(dat, plots, by = c("Destination_Plot" = "Plot")) %>%
+  rename(Dest_Salinity = Salinity, Dest_Elevation = Elevation)
+dat$Origin_Salinity <- factor(dat$Origin_Salinity, levels = c("High", "Medium", "Low"))
+dat$Origin_Elevation <- factor(dat$Origin_Elevation, levels = c("Low", "Medium", "High"))
+dat$Dest_Salinity <- factor(dat$Dest_Salinity, levels = c("High", "Medium", "Low"))
+dat$Dest_Elevation <- factor(dat$Dest_Elevation, levels = c("Low", "Medium", "High"))
 dat$Month <- month(dat$Timestamp)
 dat$Day <- day(dat$Timestamp)
-dat$Time <- time(dat$Timestamp)
+
+
+daily_dat <- dat %>%
+  group_by(Month, Day, Experiment, Destination_Plot, Dest_Salinity, Dest_Elevation,
+           Origin_Plot, Origin_Salinity, Origin_Elevation,Collar) %>%
+  summarise(n = n(), meanFlux = mean(Flux), sdFlux = sd(Flux), meanSM = mean(SMoisture), meanTemp = mean(T5))
+
+#dat$Dest_Elevation <- substr(dat$Destination_Plot, 3, 3)
+#dat$Dest_Elevation <- factor(dat$Dest_Elevation, levels = c("L", "M", "H"))
+
 # Calculate standard deviation between collars at each plot
-err <- dat %>% 
-  group_by(Month,Day, Destination_Plot, Origin_Plot, Collar) %>% 
+collar_to_collar_err <- dat %>% 
+  group_by(Month, Day, Experiment, Destination_Plot, Origin_Plot, Collar) %>% 
   summarise(n = n(), Flux = mean(Flux), Timestamp = mean(Timestamp)) %>% 
-  summarise(meanflux = mean(Flux), sdflux=sd(Flux))
+  summarise(n = n(), meanflux = mean(Flux), sdflux=sd(Flux),
+            Timestamp = mean(Timestamp), Collars = paste(Collar, collapse = " "))
 
 # Calculate CV for flux measurements
 cv <- dat %>% 
@@ -60,29 +71,23 @@ fmean <- dat %>%
   summarize(mean3 = mean(Flux), mean2 = mean(Flux[1:2]))
 
 #----- Plot time vs. flux -----
-timeflux_plot <- ggplot(dat, aes(x = Timestamp, y = Flux, color = Origin_Plot, group = Collar)) +
-  geom_point(data = dat, size = 1) +
-  geom_line(data = dat, size = 0.5) +
+timeflux_plot <- ggplot(daily_dat, aes(x = Day, y = meanFlux, color = Origin_Plot, group = Collar)) +
+  geom_point() +
+  geom_line() +
   facet_grid(Dest_Elevation ~ Dest_Salinity) +
   ggtitle("Temperature vs. Flux") +
   labs(x = "Date", y = "Flux (umol m-2 s-1)")
 print(timeflux_plot)
 ggsave("../outputs/timeflux.pdf")
 
-#geom_text_repel(data = dat, mapping = aes(x = Timestamp, y = Flux, label = Collar)) 
-#scale_color_gradientn(colors = blue2green2red(100))
-#scale_color_brewer(palette = "Set1")
-#scale_color_manual(values = c("darkolivegreen3", "coral3"))
-
 #----- Plot time vs. flux with error bars -----
-ggE <- ggplot(err, aes(x = err$Day, y = err$meanflux, color = Destination_Plot)) +
-  geom_point(data = err, size = 1) +
-  geom_line(data = err, size = 1) +
-  geom_errorbar(data = err, aes(x = err$Day, ymin = err$meanflux - err$sdflux, ymax = err$meanflux + err$sdflux), color = "black") #+
+ggE <- ggplot(collar_to_collar_err, aes(x = Timestamp, y = sdflux/meanflux, color = paste(Experiment, Origin_Plot, Destination_Plot))) +
+  geom_point() +
+  geom_line() +
+  geom_errorbar(aes(ymin = meanflux - sdflux, ymax = meanflux + sdflux), color = "black") #+
 facet_grid(Dest_Elevation ~ Dest_Salinity) #+
 theme(axis.text.x = element_text(angle = 90, hjust = 1))
-#print(ggE)
-
+print(ggE)
 
 #----- Plot temperature vs. flux with regression line -----
 q10_plot <- ggplot(dat, aes(x = T20, y = Flux)) +
@@ -91,7 +96,7 @@ q10_plot <- ggplot(dat, aes(x = T20, y = Flux)) +
   geom_smooth(method = "lm") +
   ggtitle("Temperature vs. Flux") +
   labs(x = "Temperature (oC)", y = "Flux (umol m-2 s-1)")
-#print(q10_plot)
+print(q10_plot)
 #ggsave("../outputs/q10.pdf")
 
 #----- Plot collar vs. CV with regression line -----
@@ -103,13 +108,13 @@ ggCV <- ggplot(data = cv, aes(x = Collar, y = CV, color = n)) +
 #ggsave("../outputs/cv.pdf")
 
 #----- Plot time vs. soil moisture -----
-timesm_plot <- ggplot(dat, aes(x = Timestamp, y = SMoisture, color = Origin_Plot, group = Collar)) +
-  geom_point(data = dat, size = 1) +
-  geom_line(data = dat, size = 0.5) +
+timesm_plot <- ggplot(daily_dat, aes(x = Day, y = meanSM, color = Origin_Plot, group = Collar)) +
+  geom_point() +
+  geom_line() +
   facet_grid(Dest_Elevation ~ Dest_Salinity) +
   theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
   ggtitle("Soil Moisture Over Time")
-#print(timesm_plot)
+print(timesm_plot)
 #ggsave("../outputs/timesm.pdf")
 
 #----- Plot mean flux with all 3 measurements vs. mean flux with only first two meas. -----
