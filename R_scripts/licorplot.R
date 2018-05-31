ad51a446a7196eafe47f80b282c240e8acfabdb3
Stephanie Pennington <spenn1@terpmail.umd.edu>
2018-05-31 11:04:47 -0400

    Clarify plot names

--- R_scripts/licorplot.R
+++ R_scripts/licorplot.R
@@ -27,17 +27,17 @@ collar_to_collar_err <- dat %>%
             Timestamp = mean(Timestamp), Collars = paste(Collar, collapse = " "))
 
 # Calculate CV between observations
-cv_obs <- dat %>% 
+cv_btwn_obs <- dat %>% 
   group_by(Date, Group, Collar) %>% 
   summarise(CV = sd(Flux) / mean(Flux), n = n())
 
 # Calculate CV between groups
-cv_exp <- dat %>% 
+cv_btwn_exp <- dat %>% 
   group_by(Date, Group, Experiment) %>%
   summarize(CV = sd(Flux) / mean(Flux), n = n())
 
 # Calculate mean flux of all 3 observations in the meas. and the first 2 obs. in the meas.
-fmean <- dat %>% 
+fluxMean <- dat %>% 
   group_by(Date, Group, Collar) %>%
   summarize(mean3 = mean(Flux), mean2 = mean(Flux[1:2]))
 
@@ -71,12 +71,15 @@ print(q10_plot)
 #ggsave("../outputs/q10.pdf")
 
 #----- Plot collar vs. CV with regression line -----
-ggCV <- ggplot(data = cv, aes(x = Collar, y = CV, color = n)) +
+ggCV_btwn_exp <- ggplot(data = cv_btwn_exp, aes(x = Collar, y = cv_btwn_exp, color = n)) +
   geom_point() +
   ggtitle("Coefficient of Variation")
-#geom_text_repel(data = cv, aes(label = Collar))
-print(ggCV)
-#ggsave("../outputs/cv.pdf")
+#geom_text_repel(data = cv_btwn_exp, aes(label = Collar))
+print(ggCV_btwn_exp)
+#ggsave("../outputs/cv_btwn_exp.pdf")
+
+#----- Plot CV between observations over time -----
+ggCV_btwn_obs <- ggplot(cv_btwn_obs, aes()) 
 
 #----- Plot time vs. soil moisture -----
 timesm_plot <- ggplot(daily_dat, aes(x = Date, y = meanSM, color = Group, group = Collar)) +
@@ -91,7 +94,7 @@ print(timesm_plot)
 #----- Plot mean flux with all 3 measurements vs. mean flux with only first two meas. -----
 # This is to test whether reducing observation size from 3 to 2 observations per measurement changes..
 # .. the flux
-var_test <- ggplot(fmean, aes(x = mean3, y = mean2)) + 
+var_test <- ggplot(fluxMean, aes(x = mean3, y = mean2)) + 
   geom_abline(slope = 1, intercept = 0, color = "blue") +
   geom_point() + 
   labs(x = "Mean flux of all measurements", y = "Mean flux of first 2 measurements") +
@@ -99,8 +102,7 @@ var_test <- ggplot(fmean, aes(x = mean3, y = mean2)) +
 print(var_test)
 #ggsave("../diagnostics/mean_test.png")
 
-#----- Plot CV between observations over time -----
-ggCV_obs <- ggplot(cc_obs, aes()) 
+
 
 
 figures <- list()
