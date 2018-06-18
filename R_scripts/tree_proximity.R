cb5dada57f6660a3eaddc2f49e62863f41db8a16
Stephanie Pennington <spenn1@terpmail.umd.edu>
2018-06-18 15:47:21 -0400

    Added warning, fixed DBH matchup error

--- R_scripts/tree_proximity.R
+++ R_scripts/tree_proximity.R
@@ -12,8 +12,13 @@ proxDat <- read_csv("../inventory_data/collar_proximity.csv")
 proxDat$Tag <- as.character(proxDat$Tag)
 
 # ----- Step 2: QC/error check -----
-#create error message
-#plot
+# Warning generated if duplicate tag/tree is found
+cat("Checking for duplicate trees...\n")
+if (all(duplicated(proxDat$Tag) == FALSE) == FALSE) {
+  stop(sprintf("\n Tags duplicated: %s", proxDat$Tag[duplicated(proxDat$Tag)]))
+} else {
+  cat("No duplicates found.")
+}
 
 # ----- Step 3: Read Licor and tre inventory data -----
 cat("Reading Licor data...\n")
@@ -23,8 +28,11 @@ treeDat <- read_csv("../inventory_data/inventory.csv")
 # ----- Step 4: Join datasets by collar -----
 cat("Joining datasets...\n")
 collar_to_tree_prox <- select(proxDat, -Date) %>% 
-  left_join(licorDat, by = "Collar") %>%
-  left_join(treeDat, by = "Tag")
+  left_join(treeDat, by = "Tag") %>%
+  left_join(licorDat, by = "Collar")
+  
+collar_to_tree_prox$BA_sqcm <- (pi/(4*1000))*((collar_to_tree_prox$DBH_cm)^2)
+
 
 # ----- Step 5: Plot distance vs. number of trees at each collar -----
 tree_frequency <- proxDat %>% group_by(Collar, Distance_m) %>%  
