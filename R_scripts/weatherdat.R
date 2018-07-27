e20c4e0a997f5f23cacc0d853f94db12abb0e768
Stephanie Pennington <spenn1@terpmail.umd.edu>
2018-07-27 11:40:15 -0400

    Read data and pull out s/n

--- R_scripts/weatherdat.R
+++ R_scripts/weatherdat.R
@@ -15,3 +15,7 @@ read_weather_data <- function(path) {
 }
 
 c <- read_weather_data("../weather_data/")
+
+#need to pull out sensor s/n and join with sensor inventory data
+HSLE <- read_csv("../weather_data/20377498_HSLE_20180621.csv")
+test<- grep("SEN S/N:", HSLE)
