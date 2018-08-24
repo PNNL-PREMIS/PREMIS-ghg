f75020f98ac92bc6f80f3bd2b5de84bbfbe5cd82
Stephanie Pennington <spenn1@terpmail.umd.edu>
2018-08-24 12:58:41 -0400

    Preliminary plots for wx data

--- R_scripts/weatherdat.R
+++ R_scripts/weatherdat.R
@@ -13,24 +13,39 @@ read_wxdat <- function(file) {
   subfile <- read_csv("../inventory_data/wstation_info.csv")
   wdat <- file %>% 
     read_csv(skip=1) %>%
-    gather(label, value, -1:-2) %>% 
+    gather(label, Value, -1:-2) %>% 
     separate(label, sep = "SEN S/N: ", into = c("firstpart", "Sensor_SN")) %>%
-    separate(firstpart, sep = ",", into = c("SensorType", "info")) %>%
+    separate(firstpart, sep = ",", into = c("Sensor_Group", "info")) %>%
     select(- info)
   wdat$Timestamp <- mdy_hms(wdat$`Date Time, GMT-04:00`)
   wdat$Sensor_SN <- as.integer(gsub(")", "", wdat$Sensor_SN))
  
   wdat <- left_join(wdat, subfile, by = "Sensor_SN") 
-  wdat <- select(wdat, -`Date Time, GMT-04:00`)
+  wdat <- separate(wdat, Sensor_Label, sep = "_", into = c("info1", "info2", "Sensor_Depth"))
+  wdat <- select(wdat, -`Date Time, GMT-04:00`, -info1, -info2)
+
 }
 
-LSLE <- read_wxdat("../weather_data/20377496_LSLE_20180718.csv")
-MSLE <- read_wxdat("../weather_data/20377497_MSLE_20180718.csv")
-HSLE <- read_wxdat("../weather_data/20377498_HSLE_20180718.csv")
+LSLE <- read_wxdat("../weather_data/LSLE_weather_20180809.csv")
+MSLE <- read_wxdat("../weather_data/MSLE_weather_20180809.csv")
+HSLE <- read_wxdat("../weather_data/HSLE_weather_20180806.csv")
 
+all_sites <- bind_rows(LSLE, MSLE, HSLE)
 
+#filer()
 ggplot(data = HSLE, aes(x = Timestamp, group = Sensor_Type)) + 
   geom_line(aes(y = SensorType == "Temp"))
+
+ggplot(filter(all_sites, Sensor_Group == "Water Content"), aes(Timestamp, Value, color = Site, group = Sensor_SN)) + 
+  facet_wrap(~Sensor_Depth) + 
+  geom_line()
+
+ggplot(filter(all_sites, Sensor_Type == "TRH"), aes(Timestamp, Value, color = Sensor_Group)) + 
+  geom_line()
+
+ggplot(filter(all_sites, Sensor_Group == "Temp"), aes(Timestamp, Value, color = Site, group = Sensor_SN)) + 
+  facet_wrap(~Sensor_Depth) + 
+  geom_line()
 
 # Calculate stdev for each temperature and moisture probe
 qc_weather <- HSLE %>%
