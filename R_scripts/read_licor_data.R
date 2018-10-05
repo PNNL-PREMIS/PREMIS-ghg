74564599c04d58e9425d8665979e21342da83232
Ben Bond-Lamberty <bondlamberty@pnnl.gov>
2018-10-05 14:37:18 -0400

    Squash some blank-line bugs

--- R_scripts/read_licor_data.R
+++ R_scripts/read_licor_data.R
@@ -6,20 +6,14 @@ read_licor_data <- function(filename) {
   nobs <- length(file[grepl("^Obs#:", file)])
   cat("Reading...", filename, " lines =", length(file), "observations =", nobs, "\n")
   
-  record_sep <- grep(pattern = "^$", file)
+  record_starts <- grep(pattern = "^LI-8100", file)
   
   find_parse <- function(tabletext, lbl) {
     line <- tail(grep(lbl, tabletext), n = 1)
     if(length(line)) gsub(lbl, "", tabletext[line]) else ""
   }
-  label <- file[grepl("^Label:", file)]  # Pull out variables
-  flux <- file[grepl("^Lin_Flux:", file)]
-  r2 <- file[grepl("^Lin_R2:", file)]
-  date <- file[which(grepl("^Type", file)) + 1]
-  comments <- file[grepl("^Comments:", file)]
-  port <- file[grepl("^Port#:", file)]  # may or may not be present
   
-  results <- tibble(table = seq_along(record_sep),
+  results <- tibble(table = seq_along(record_starts),
                     Timestamp = as_datetime(NA),
                     Label = NA_character_,
                     Port = NA_integer_,
@@ -27,50 +21,55 @@ read_licor_data <- function(filename) {
                     R2 = NA_character_,
                     T5 = NA_character_,
                     Tcham = NA_character_,
-                    SMoist = NA_character_,
+                    SMoisture = NA_character_,
                     Comments = NA_character_)
   
-  previous_table_end <- 0
-  for (i in seq_along(record_sep)) {
-    tabletext <- file[(previous_table_end + 1):record_sep[i]]
-    #cat(i, length(tabletext), " ")
-    #browser()
+  for (i in seq_along(record_starts)) {
+    if(i < length(record_starts)) {
+      record_end <- record_starts[i+1]-1 
+    } else {
+      record_end <- length(file)
+    }
+    record <- file[record_starts[i]:record_end]
+    # Get rid of blank lines because that can screw up paste(collapse()) below
+    record <- record[grep("^$", record, invert = TRUE)]
+    cat(i, record_starts[i], ":", record_end, length(record), "\n")
+    
     # Find the table start and stop
-    table_start <- tail(grep("^Type\t", tabletext), n = 1)
-    table_stop <- tail(grep("^CrvFitStatus:\t", tabletext), n = 1)
+    table_start <- tail(grep("^Type\t", record), n = 1)
+    table_stop <- tail(grep("^CrvFitStatus:\t", record), n = 1)
     if(length(table_stop) == 0) {
-      message("Skipping table", i, previous_table_end + 1:record_sep[i])
+      message("Skipping table ", i, " ", record_starts[i], ":", record_end)
       next()
     }
     # Find names, discarding any trailing 'Annotation' column
-    col_names <- strsplit(file[table_start], "\t", fixed = TRUE)[[1]]
+    col_names <- strsplit(record[table_start], "\t", fixed = TRUE)[[1]]
     col_names <- col_names[!grepl("Annotation", col_names)]
     #cat("\tReading table at", table_start, ":", tablestops[i], "...\n")
-    tabletext[(table_start+1):(table_stop-1)] %>% 
+    record[(table_start+1):(table_stop-1)] %>% 
       paste(collapse = "\n") %>% 
       readr::read_tsv(col_names = col_names) ->
       df
     
     index <- which(df$Type == 1)
     results$Timestamp[i] <- mean(df$Date)
-    results$Label[i] <- find_parse(tabletext, "^Label:\t")
-    results$Port[i] <- find_parse(tabletext, "^Port#:\t")
-    results$Flux[i] <- find_parse(tabletext, "^Exp_Flux:\t")
-    results$R2[i] <- find_parse(tabletext, "^Exp_R2:\t")
+    results$Label[i] <- find_parse(record, "^Label:\t")
+    results$Port[i] <- find_parse(record, "^Port#:\t")
+    results$Flux[i] <- find_parse(record, "^Exp_Flux:\t")
+    results$R2[i] <- find_parse(record, "^Exp_R2:\t")
     results$T5[i] <- mean(df$V4[index])
     results$Tcham[i] <- mean(df$Tcham[index])
-    results$SMoist[i] <- mean(df$V3[index])
-    results$Comments[i] <- find_parse(tabletext, "^Comments:\t")
-    previous_table_end <- record_sep[i]
+    results$SMoisture[i] <- mean(df$V3[index])
+    results$Comments[i] <- find_parse(record, "^Comments:\t")
   }
-
+  
   results %>% 
     mutate(Port = as.integer(Port),
            Flux = as.numeric(Flux),
            R2 = as.numeric(R2),
            T5 = as.numeric(T5),
            Tcham = as.numeric(Tcham),
-           SMoist = as.numeric(SMoist))
+           SMoisture = as.numeric(SMoisture))
 }
 
 #----- Function to loop through directory and call function to read licor data -----
