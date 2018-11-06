4f3208ba2b1739f82521f018a2cb508244c23bc6
Stephanie Pennington <spenn1@terpmail.umd.edu>
2018-11-06 16:51:29 -0500

    Changed plot scale

--- R_scripts/licorplot.R
+++ R_scripts/licorplot.R
@@ -130,7 +130,7 @@ print(ggCV_btwn_obs)
 
 #----- Plot time vs. soil moisture -----
 timesm_plot <- ggplot(daily_dat, aes(x = Timestamp, y = meanSM, color = Group, group = Collar)) +
-  ylim(0,0.6) +
+  ylim(0,2) +
   geom_point() +
   geom_line() +
   facet_grid(Dest_Elevation ~ Dest_Salinity) +
