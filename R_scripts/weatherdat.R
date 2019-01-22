c681b686ad6057f94e546300e81ab128f213e297
Stephanie Pennington <spenn1@terpmail.umd.edu>
2019-01-22 13:34:47 -0500

    Fix select() issue

--- R_scripts/weatherdat.R
+++ R_scripts/weatherdat.R
@@ -9,7 +9,7 @@ read_wxdat <- function(filename, wstation_info) {
     gather(label, Value, -1:-2) %>%  # first two columns are descriptive
     separate(label, sep = ", SEN S/N: ", into = c("firstpart", "Sensor_SN")) %>% # separate, remove unwanted info
     separate(firstpart, sep = ",", into = c("Sensor_Group", "info")) %>% 
-    select(-info) ->
+    dplyr::select(-info) ->
     wdat
   wdat$Timestamp <- mdy_hms(wdat$`Date Time, GMT-04:00`)
   wdat$Sensor_SN <- as.integer(gsub(")", "", wdat$Sensor_SN))
@@ -17,7 +17,7 @@ read_wxdat <- function(filename, wstation_info) {
   wdat %>% 
     left_join(wstation_info, by = "Sensor_SN") %>%  # Join data with station information
     separate(Sensor_Label, sep = "_", into = c("info1", "info2", "Sensor_Depth"), fill = "right") %>% 
-    select(-`Date Time, GMT-04:00`, -info1, -info2)
+    dplyr::select(-`Date Time, GMT-04:00`, -info1, -info2)
 }
 
 # Read all available weather or well station files, combine, and remove duplicate rows
@@ -40,7 +40,7 @@ read_single_well <- function(filename) {
            col_names = c("#", "Timestamp", "Low_Range", "High_Range", "Temp",
                          paste0("X", 1:8)),
            col_types = "icdddcccccccc") %>% 
-    select(Timestamp, Low_Range, High_Range, Temp) %>% 
+    dplyr::select(Timestamp, Low_Range, High_Range, Temp) %>% 
     mutate(Timestamp = mdy_hms(Timestamp),
            Plot = plot)
 }
