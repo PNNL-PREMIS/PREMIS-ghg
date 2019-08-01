63d37640d09b2343fef2b29c6fa4147603418c4c
Lillie Haddock <lillie.haddock@pnnl.gov>
2019-08-01 16:49:01 -0400

    Created read_litter_data function

--- R_scripts/litter.R
+++ R_scripts/litter.R
@@ -1,7 +1,18 @@
-## Script to process and plot litter data
+## Function to read in a directory of litter data to combine in a long format for data visualization
 ## Created 2019-05-29
 
 library(readr)
 library(ggplot2)
+library(dplyr)
 
-litter_data <- list.files(path = "../litter_data/") %>% lapply(read_csv)
+read_litter_data <- function(path) {
+# Read in litter directory and combine to one data frame
+    litter_data <- list.files(path = path, full.names = TRUE) %>% 
+    lapply(read_csv) %>% 
+    bind_rows()
+    
+ #select useful columns 
+    litter_data %>% 
+      gather(key = "Litter_Type", value = "Mass_g", M_woody:M_leaf_other) %>% 
+      select(-Date_weighed, -Notes)
+}
